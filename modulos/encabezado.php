b194ae0aa3b5e282c986beba26a97b8467e45092
Facundo Delgado <fakundo.delgado@gmail.com>
2026-08-25 18:28:23 -0300

    Estructura base de mi sitio con PHP.
    Cree dos módulos, uno para el header y otro para el footer. De esa forma evito tener que "pegarlo" en cada página de mi sitio. Creé el primer archivo .css aunque no contiene mucho, y agregué un par de imágenes, una del favicon y otra extra para el contenido de la página principal.

---
+++ modulos/encabezado.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Mi sitio web</title>
+        <link rel="icon" type="image/png" href="recursos/imagenes/favicon.png">
+        <link rel="stylesheet" href="recursos/css/principal.css">
+    </head>
+    <body>
+        <header>
+            <div class="encabezado">
+                <span class="titulo-sitio">Mi sitio personal</span>
+                <span class="autor">Delgado Facundo</span>
+            </div> 
+            <nav>
+            <a href="academico.php">Formación</a>
+            <a href="index.php">Presentación</a>
+            <a href="contacto.php">Contacto</a>
+            </nav> 
+        </header>
---
+++ modulos/pie_de_pagina.php
@@ -0,0 +1,26 @@
+<footer>
+            <div class="contactos">
+                <p>Medios de contacto: </p>
+                <ul>
+                    <li>Teléfono: 1162297804</li>
+                    <li>Email: <a href="mailto:fakundo.delgado@gmail.com">fakundo.delgado@gmail.com</a></li>
+                    <li><a href="contacto.php">Formulario de contacto</a></li>
+                </ul>
+            </div>
+            <div class="relacionado">
+                 <p>Páginas de interés: </p>
+                <dl>
+                    <dt>Repositorio: </dt><dd><a href="https://github.com/fakundodelgado">Github</a></dd>
+                    <dt>Universidad: </dt><dd><a href="https://www.unsa.edu.ar/">UNSa</a></dd>
+                    <dt>Facultad: </dt><dd><a href="https://exactas.unsa.edu.ar/">Facultad de Ciencias Exactas</a></dd>
+                </dl>
+            </div>
+           <div class="legal">
+                <p>
+                Este sitio web fue desarrollado con fines exclusivamente académicos para la carrera Tecnicatura Universitaria en Programación (UNSa). 
+                Las marcas, logotipos e imágenes pertenecen a sus respectivos autores y se utilizan con fines ilustrativos.
+                </p>
+           </div>
+        </footer>
+    </body>
+</html>
