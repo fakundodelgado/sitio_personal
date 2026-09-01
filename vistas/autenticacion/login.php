82f973dd1ce8c746999008a6832a18433455892b
Facundo Delgado <fakundo.delgado@gmail.com>
2026-09-01 14:24:18 -0300

    Prácticamente sin cambios.
    Me disponía a continuar aplicando las clases de grid a mi HTML pero dado que por el estilo dado a los formularios y el trabajo realizado sobre el main todas las páginas quedaron centradas como quería no veo razón para continuar con eso, aunque sea de momento. Al ser la primera vez haciendo tanto CSS quizás sea mejor leer de nuevo para detectar redundancias e intentar reducir un poco la cantidad de líneas.

--- vistas/autenticacion/login.php
+++ vistas/autenticacion/login.php
@@ -1,31 +1,31 @@
 <?php include __DIR__ . '/../plantillas/encabezado.php'; ?> 
 
 <body>
-<main>
-<h1>Iniciar sesión</h1>
+    <main>
+        <h1>Iniciar sesión</h1>
 
-<?php if (!empty($error)): ?>
-    <div class="error"><?= htmlspecialchars($error) ?></div>
-<?php endif; ?>
+        <?php if (!empty($error)): ?>
+            <div class="error"><?= htmlspecialchars($error) ?></div>
+        <?php endif; ?>
 
-<form method="POST" action="?action=procesar_login" class="formulario-login">
-    <fieldset>
-    <legend>Login</legend>
-    <label for="usuario" >Usuario: </label>
-    <input id="usuario" type="text" name="usuario" required placeholder="Ingrese usuario...">
+        <form method="POST" action="?action=procesar_login" class="formulario-login">
+            <fieldset>
+            <legend>Login</legend>
+            <label for="usuario" >Usuario: </label>
+            <input id="usuario" type="text" name="usuario" required placeholder="Ingrese usuario...">
 
-    <label for="contrasena">Contraseña: </label>
-    <input id="contrasena" type="password" name="contrasena" required placeholder="Ingrese contraseña..."...>
+            <label for="contrasena">Contraseña: </label>
+            <input id="contrasena" type="password" name="contrasena" required placeholder="Ingrese contraseña...">
 
-    <button type="submit">Ingresar</button>
-    </fieldset>
-</form>
+            <button type="submit">Ingresar</button>
+            </fieldset>
+        </form>
 
-<div class="datos">
-    <p><span id="top">Datos de prueba</span></p>
-    <p><span>Usuario:</span> profesor</p>
-    <p><span>Contraseña:</span> 123456</p>
-</div>
-</main>
+        <div class="datos">
+            <p><span id="top">Datos de prueba</span></p>
+            <p><span>Usuario:</span> profesor</p>
+            <p><span>Contraseña:</span> 123456</p>
+        </div>
+    </main>
 
 <?php include __DIR__ . '/../plantillas/pie_de_pagina.php'; ?>
